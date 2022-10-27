e58af816a6c70e5550f5145db612947c44526094
fateme <miss.l3lue@gmail.com>
2022-10-27 23:43:31 +0330

    1

--- src/HomePage.js
+++ src/HomePage.js
@@ -1,15 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function HomePage() {
+  const [keyword, setKeyword] = useState("");
+  const [src, setSrc] = useState("");
+
+  function handleChange(e) {
+    setKeyword(e.target.value);
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    getMusic();
+  }
+
+  async function getMusic() {
+    const res = await axios.get("http://localhost:3000/musics", {
+      params: {
+        test: keyword,
+      },
+    });
+    console.log(res);
+  }
+
   return (
     <div className="HomePage text-center container border py-5">
       <h1>جستجوگر آهنگ</h1>
       <p>میتونی بخشی از آهنگ مورد علاقه ات رو وارد کنی و پیداش کنی</p>
-      <form>
+      <form method="get" onSubmit={handleSubmit}>
         <div className="row justify-content-center">
           <div className="col-12 col-md-6">
-            <input className="form-control text-end" />
+            <input onChange={handleChange} className="form-control text-end" />
           </div>
         </div>
         <div className="row justify-content-center mt-3">
@@ -21,6 +43,7 @@ export default function HomePage() {
         </div>
       </form>
       <Link to={`/gorbe`}>router test link</Link>
+      <img className="d-block" src={src} />
     </div>
   );
 }
